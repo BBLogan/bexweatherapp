d280aab06edc2222aa3a45eca4c31c38b9bd6797
BexLogan <bek.jayne@gmail.com>
2023-11-09 20:20:14 +0800

    updating test files & folders - 1st and 3rd solved

--- weather.py
+++ weather.py
@@ -3,7 +3,10 @@ from datetime import datetime
 
 DEGREE_SYBMOL = u"\N{DEGREE SIGN}C"
 
+# READ THE DOC STRINGS - THE FORMULA IS THE ONE YOU'RE USING AT THE TOP OF EACH 
 
+
+# this one done
 def format_temperature(temp):
     """Takes a temperature and returns it in string format with the degrees
         and celcius symbols.
@@ -16,6 +19,7 @@ def format_temperature(temp):
     return f"{temp}{DEGREE_SYBMOL}"
 
 
+# do this one 5th - google the words in the mulitple "" - docstrings - CSV examples
 def convert_date(iso_string):
     """Converts and ISO formatted date into a human readable format.
 
@@ -26,8 +30,11 @@ def convert_date(iso_string):
     """
     pass
 
-
+# this one done
 def convert_f_to_c(temp_in_farenheit):
+    celcius = round((float(temp_in_farenheit) - 32) * (5/9), 1)
+    return celcius
+
     """Converts an temperature from farenheit to celcius.
 
     Args:
@@ -37,8 +44,9 @@ def convert_f_to_c(temp_in_farenheit):
     """
     pass
 
-
+# do this one 4th - what's the mean forumla  - REMEMBER TO CHANGE WHICH TEST FILES ARE ACTIVE AND WHICH AREN'T run_tests.py - for this one to pass look at what the number types are in the test_calculate_mean.py file
 def calculate_mean(weather_data):
+    
     """Calculates the mean value from a list of numbers.
 
     Args:
@@ -48,8 +56,27 @@ def calculate_mean(weather_data):
     """
     pass
 
-
+# do this one 3rd - just open a CSV file  - REMEMBER TO CHANGE WHICH TEST FILES ARE ACTIVE AND WHICH AREN'T run_tests.py
 def load_data_from_csv(csv_file):
+    data = [] 
+    # the above starts an empty list to store the CSV data
+
+    try:
+        with open(csv_file, mode='r', newline='', encoding="utf-8") as file:
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                data.append(row)
+        
+        return data
+    except FileNotFoundError:
+        # handle if file doesn't exist
+        return None
+    
+    except Exception as e:
+        # any other errors which may occur during file reading
+        print("An error occurred: ", str(e))
+        return None
+    
     """Reads a csv file and stores the data in a list.
 
     Args:
@@ -59,7 +86,7 @@ def load_data_from_csv(csv_file):
     """
     pass
 
-
+# do this one 6th - and then follow on with the max (max = inverse of this one) - REMEMBER TO CHANGE WHICH TEST FILES ARE ACTIVE AND WHICH AREN'T run_tests.py
 def find_min(weather_data):
     """Calculates the minimum value in a list of numbers.
 
@@ -70,7 +97,7 @@ def find_min(weather_data):
     """
     pass
 
-
+# do this one 7th - after the find_min
 def find_max(weather_data):
     """Calculates the maximum value in a list of numbers.
 
@@ -81,7 +108,7 @@ def find_max(weather_data):
     """
     pass
 
-
+# need to do the ones above before attempting this one
 def generate_summary(weather_data):
     """Outputs a summary for the given weather data.
 
@@ -92,7 +119,7 @@ def generate_summary(weather_data):
     """
     pass
 
-
+# need to do the ones above before attempting this one
 def generate_daily_summary(weather_data):
     """Outputs a daily summary for the given weather data.
 
